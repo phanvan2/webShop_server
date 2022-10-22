c565b5256e81c28510ca1d39aa99951a81daf390
vanduc19it <duc19it@gmail.com>
2022-10-22 15:41:12 +0700

    errorr

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 import  express  from "express";
+import cors  from "cors";
 import bodyParser from "body-parser";
 import initRoutes  from "./routes/web"; 
 import connectDB from "./config/connectDB";
-const app = express();
 
+const app = express()
+const port = 5000;
+
+app.use(cors());
 
 connectDB();
 app.use(express.json());
@@ -12,5 +16,5 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 initRoutes(app);
 
-app.listen(5000);
+app.listen(port);
 
